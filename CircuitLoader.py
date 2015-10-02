fa55fa8818296d8a4a06985f67ae7de407d53c23
Rémi Bèges <remi.beges@gmail.com>
2015-10-02 23:44:46 +0200

    Added circuit loader code and circuit example
    Circuit is loaded with black and white image. Line contour is then
    recomputed using OpenCV (cv2) module

---
+++ CircuitLoader.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+def load_circuit(filename):
+    im =  cv2.imread(filename,0)
+    ret,thresh = cv2.threshold(im,127,255,0)
+    contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+
+    xt = []
+    yt = []
+
+    for coords in contours[1][1]:
+        xt.append(coords[0][0])
+        yt.append(coords[0][1])
+
+    x = np.array(xt)
+    y = np.array(yt)
+
+    return x,y
